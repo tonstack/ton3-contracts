5aa8dc0d5b3a556bc5e772121397d68e3b6d4449
cryshado <mirmiclub@gmail.com>
2022-04-10 01:22:52 +0300

    add gpl v3 header

--- src/wallets/highload-wallet-v2.ts
+++ src/wallets/highload-wallet-v2.ts
@@ -1,3 +1,24 @@
+/*
+ton3-contracts – JS package for interacting with TON contracts
+
+Copyright (C) 2022 TonStack <https://github.com/tonstack>
+
+This file is part of ton3-contracts.
+
+ton3-contracts is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ton3-contracts is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ton3-contracts.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import {
     Bit,
     Builder,
